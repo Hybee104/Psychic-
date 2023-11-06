1856bb705a46f138c8fde8b5d413ea77965713a2
Hybee104 <113289136+Hybee104@users.noreply.github.com>
2023-11-06 15:58:07 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+
+            function scrolling(div) {
+                $('html,body').animate(
+                   {
+                      scrollTop: $("#" + div).offset().top
+                   }, 'slow');
+             }
+
+                  // alert(" Get a free reading xxx ")
